9ae0ff2acdcd2477c7f382bd1bde1fd3cfe16b61
«Muzhait» <muzhait@mail.ru>
2023-01-27 00:54:46 +0500

    dz 1 lesson

---
+++ dz1.2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+
+Console.Write("Введите число a; ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число b; ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число c; ");
+int c = int.Parse(Console.ReadLine()!);
+
+if (a > b & a > c)
+{
+    Console.WriteLine($"Максимальное число {a}");
+}
+else if (b > a & b > c)
+{
+    Console.WriteLine($"Максимальное число {b}");
+}
+else{
+    Console.WriteLine($"Максимальное число {c}");
+}
