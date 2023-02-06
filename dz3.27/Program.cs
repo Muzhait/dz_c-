8b901f2c791e856cea88e1d72402ec2c49391129
«Muzhait» <muzhait@mail.ru>
2023-02-07 00:27:47 +0500

    4 seminar

---
+++ dz3.27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+int GetSum(int A)
+{
+    int sum = 0;
+    for (int dig = 0; A > 0; A = A / 10)
+    {
+        dig = A % 10;
+        sum = sum + dig;
+    }
+    return sum;
+}
+
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма цифр числа {N} равна {GetSum(N)}");
